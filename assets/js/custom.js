5dcb7427d55e63c7c7244c049826254ee1440d50
Chris Hsu <yaostyle@gmail.com>
2022-03-30 22:38:05 -0700

    Update js file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,4 +5,20 @@ window.onload = function(){
    let school_starts = document.getElementById("school_starts");
    school_starts.innerText = "2022/04/04";
 
+   // TO-DO: Add comments dynamically
+   // let comments = [];
+   // // 222
+   // comments.push({
+   //    module: "ipt1",
+   //    name: "Chris",
+   //    content: "comment 1",
+   //    date: "03/30/2022"
+   // });
+
+   // comments.push({
+   //    module: "dm",
+   //    name: "Chris",
+   //    content: "comment 2",
+   //    date: "03/30/2022"
+   // });
 }  
