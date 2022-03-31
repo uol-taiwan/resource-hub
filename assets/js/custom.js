d7ff0e89e37d1ef90497273461bbba69e4c4d31e
Chris Hsu <yaostyle@gmail.com>
2022-03-30 22:25:56 -0700

    Update js file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,8 @@
 window.onload = function(){  
    let copyright = document.getElementsByClassName("copyright");
    copyright[0].prepend("© UoL Taiwan ");
+
+   let school_starts = document.getElementById("school_starts");
+   school_starts.innerText("04/04/2022");
+
 }  
